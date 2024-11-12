f14178b397633f3c4783f5879d1062b3f33dc926
mariu <mariu@DESKTOP-CT70FK7>
2024-11-12 18:54:38 +0100

    Add project files.

---
+++ uppgift3.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uppgift_3._4
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Låtens längd i minuter och sekunder
+            Console.Write("Ange låtens längd i minuter: ");
+            int minuter = int.Parse(Console.ReadLine());
+
+            Console.Write("Ange låtens längd i sekunder: ");
+            int sekunder = int.Parse(Console.ReadLine());
+
+            if ((minuter == 2 && sekunder >= 45) || (minuter > 2 && minuter < 4) || (minuter == 4 && sekunder <= 20))
+            {
+                Console.WriteLine("Låten får spelas på radiostationen.");
+            }
+            else
+            {
+                Console.WriteLine("Låten får inte spelas på radiostationen.");
+            }
+        }
+    }
+}
